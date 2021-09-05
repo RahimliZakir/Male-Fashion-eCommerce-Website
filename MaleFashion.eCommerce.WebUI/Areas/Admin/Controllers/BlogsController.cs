4fed6febd46fd187e31d9bd04dd0a7d34a3e0b81
RahimliZakir <zakirer@code.edu.az>
2021-09-05 18:09:36 +0400

    Blogs Side Area Config

---
+++ MaleFashion.eCommerce.WebUI/Areas/Admin/Controllers/BlogsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MaleFashion.eCommerce.WebUI.Models.DataContext;
+using MaleFashion.eCommerce.WebUI.Models.Entity;
+
+namespace MaleFashion.eCommerce.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogsController : Controller
+    {
+        private readonly FashionDbContext _context;
+
+        public BlogsController(FashionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Blogs
+        public async Task<IActionResult> Index()
+        {
+            var fashionDbContext = _context.Blogs.Include(b => b.Aphorism);
+            return View(await fashionDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Blogs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs
+                .Include(b => b.Aphorism)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // GET: Admin/Blogs/Create
+        public IActionResult Create()
+        {
+            ViewData["AphorismId"] = new SelectList(_context.Aphorisms, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Blogs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Title,Description,ImagePath,AphorismId,AuthorImagePath,AuthorName,AuthorSurname,Id,CreatedDate,UpdatedDate,DeletedDate")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blog);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AphorismId"] = new SelectList(_context.Aphorisms, "Id", "Id", blog.AphorismId);
+            return View(blog);
+        }
+
+        // GET: Admin/Blogs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            ViewData["AphorismId"] = new SelectList(_context.Aphorisms, "Id", "Id", blog.AphorismId);
+            return View(blog);
+        }
+
+        // POST: Admin/Blogs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Title,Description,ImagePath,AphorismId,AuthorImagePath,AuthorName,AuthorSurname,Id,CreatedDate,UpdatedDate,DeletedDate")] Blog blog)
+        {
+            if (id != blog.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blog);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogExists(blog.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AphorismId"] = new SelectList(_context.Aphorisms, "Id", "Id", blog.AphorismId);
+            return View(blog);
+        }
+
+        // GET: Admin/Blogs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs
+                .Include(b => b.Aphorism)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // POST: Admin/Blogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+            _context.Blogs.Remove(blog);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogExists(int id)
+        {
+            return _context.Blogs.Any(e => e.Id == id);
+        }
+    }
+}
