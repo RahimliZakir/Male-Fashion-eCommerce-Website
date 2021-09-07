090c2648685dc5241e9e2f5c45cf2ba2eb0700d9
RahimliZakir <zakirer@code.edu.az>
2021-09-07 11:13:42 +0400

    Personal Side Created And Bug Fixes

--- MaleFashion.eCommerce.WebUI/Areas/Admin/Controllers/PersonalSideController.cs
+++ MaleFashion.eCommerce.WebUI/Areas/Admin/Controllers/PersonalSideController.cs
@@ -1,11 +1,17 @@
 ﻿using MaleFashion.eCommerce.WebUI.AppCode.Extensions;
+using MaleFashion.eCommerce.WebUI.Areas.Admin.Models.FormModel;
 using MaleFashion.eCommerce.WebUI.Models.DataContext;
 using MaleFashion.eCommerce.WebUI.Models.Entity.Membership;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace MaleFashion.eCommerce.WebUI.Areas.Admin.Controllers
 {
@@ -13,10 +19,14 @@ namespace MaleFashion.eCommerce.WebUI.Areas.Admin.Controllers
     public class PersonalSideController : Controller
     {
         private readonly FashionDbContext db;
+        private readonly IWebHostEnvironment env;
+        private readonly UserManager<AppUser> userManager;
 
-        public PersonalSideController(FashionDbContext db)
+        public PersonalSideController(FashionDbContext db, UserManager<AppUser> userManager, IWebHostEnvironment env)
         {
             this.db = db;
+            this.env = env;
+            this.userManager = userManager;
         }
 
         public IActionResult Index()
--- MaleFashion.eCommerce.WebUI/Models/Entity/Membership/AppUser.cs
+++ MaleFashion.eCommerce.WebUI/Models/Entity/Membership/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,5 +20,8 @@ namespace MaleFashion.eCommerce.WebUI.Models.Entity.Membership
         public string JobName { get; set; }
 
         public int Age { get; set; }
+
+        [NotMapped]
+        public string ImageTemp { get; set; }
     }
 }
