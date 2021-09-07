cfaf7a51b6134762c17585d432bca33f135caa7b
RahimliZakir <zakirer@code.edu.az>
2021-09-07 09:29:45 +0400

    Serilog Implementation Done

--- MaleFashion.eCommerce.WebUI/Program.cs
+++ MaleFashion.eCommerce.WebUI/Program.cs
@@ -1,9 +1,10 @@
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,11 +38,34 @@ namespace MaleFashion.eCommerce.WebUI
                          .Distinct()
                          .ToArray();
 
+            //Read Configuration from appSettings
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            //Initialize Logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Proqram işə düşür.");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Proqram işə düşən zaman xəta baş verdi.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog() //Uses Serilog instead of default .NET Logger
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
