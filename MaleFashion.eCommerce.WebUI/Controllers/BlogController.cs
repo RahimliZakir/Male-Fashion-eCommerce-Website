ff36c2246bb6ba19eab71e9112e372ccf6c758da
RahimliZakir <zakirer@code.edu.az>
2021-09-09 09:40:08 +0400

    Like & Unlike Redirect Logic Done

--- MaleFashion.eCommerce.WebUI/Controllers/BlogController.cs
+++ MaleFashion.eCommerce.WebUI/Controllers/BlogController.cs
@@ -1,5 +1,7 @@
-﻿using MaleFashion.eCommerce.WebUI.Models.DataContext;
+﻿using MaleFashion.eCommerce.WebUI.AppCode.Extensions;
+using MaleFashion.eCommerce.WebUI.Models.DataContext;
 using MaleFashion.eCommerce.WebUI.Models.Entity;
+using MaleFashion.eCommerce.WebUI.Models.Entity.Membership;
 using MaleFashion.eCommerce.WebUI.Models.ViewModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -25,9 +27,84 @@ namespace MaleFashion.eCommerce.WebUI.Controllers
         {
             var data = db.Blogs.Include(l => l.Likes).Include(u => u.Unlikes).OrderBy(b => b.Id);
 
+            // Limited Paged ViewModel
             var pagedViewModel = new PagedViewModel<Blog>(data, pageIndex, pageSize);
 
             return View(pagedViewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        async public Task<IActionResult> Like(int blogId)
+        {
+            AppUser currentUser = await db.Users.FirstOrDefaultAsync(u => u.Id == User.GetUserId());
+            int currentUserId = User.GetUserId();
+
+            if (!db.Likes.Any(l => l.UserId == currentUserId && l.BlogId == blogId))
+            {
+                var like = new Like
+                {
+                    BlogId = blogId,
+                    UserId = currentUserId
+                };
+
+                try
+                {
+                    await db.Likes.AddAsync(like);
+                    await db.SaveChangesAsync();
+                }
+                catch (Exception)
+                {
+                    return Json(new
+                    {
+                        error = true,
+                        message = "Xeta bash verdi, bir neche-deqiqeden sonra yeniden yoxlayin!"
+                    });
+                }
+            }
+
+            return Json(new
+            {
+                error = false,
+                likeCount = db.Likes.Count(l => l.BlogId == blogId)
+            });
+        }
+
+        [HttpPost]
+        [Authorize]
+        async public Task<IActionResult> Unlike(int blogId)
+        {
+            AppUser currentUser = await db.Users.FirstOrDefaultAsync(u => u.Id == User.GetUserId());
+            int currentUserId = User.GetUserId();
+
+            if (!db.Unlikes.Any(u => u.UserId == currentUserId && u.BlogId == blogId))
+            {
+                var unlike = new Unlike
+                {
+                    BlogId = blogId,
+                    UserId = currentUserId
+                };
+
+                try
+                {
+                    await db.Unlikes.AddAsync(unlike);
+                    await db.SaveChangesAsync();
+                }
+                catch (Exception)
+                {
+                    return Json(new
+                    {
+                        error = true,
+                        message = "Xeta bash verdi, bir neche-deqiqeden sonra yeniden yoxlayin!"
+                    });
+                }
+            }
+
+            return Json(new
+            {
+                error = false,
+                unlikeCount = db.Unlikes.Count(l => l.BlogId == blogId)
+            });
         }
     }
 }
