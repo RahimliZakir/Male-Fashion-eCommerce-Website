e29afd775e08e610889b63fe022211361462a31e
RahimliZakir <zakirer@code.edu.az>
2021-09-10 07:04:01 +0400

    Discount Campaigns Area Side Done

--- MaleFashion.eCommerce.WebUI/Areas/Admin/Controllers/CampaignsController.cs
+++ MaleFashion.eCommerce.WebUI/Areas/Admin/Controllers/CampaignsController.cs
@@ -98,6 +98,7 @@ namespace MaleFashion.eCommerce.WebUI.Areas.Admin.Controllers
             {
                 try
                 {
+                    campaign.UpdatedDate = DateTime.UtcNow.AddHours(4);
                     _context.Update(campaign);
                     await _context.SaveChangesAsync();
                 }
