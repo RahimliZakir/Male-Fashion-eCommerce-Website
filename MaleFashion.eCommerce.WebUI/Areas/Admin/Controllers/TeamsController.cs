1a3fc1c3883b9e17b4d630fcf211b6fb2d681885
RahimliZakir <zakirer@code.edu.az>
2021-10-19 07:19:03 +0400

    Teams & TeamJobs Area Side Created

---
+++ MaleFashion.eCommerce.WebUI/Areas/Admin/Controllers/TeamsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MaleFashion.eCommerce.WebUI.Models.DataContext;
+using MaleFashion.eCommerce.WebUI.Models.Entity;
+
+namespace MaleFashion.eCommerce.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TeamsController : Controller
+    {
+        private readonly FashionDbContext _context;
+
+        public TeamsController(FashionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Teams
+        public async Task<IActionResult> Index()
+        {
+            var fashionDbContext = _context.Teams.Include(t => t.TeamJob);
+            return View(await fashionDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Teams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.TeamJob)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // GET: Admin/Teams/Create
+        public IActionResult Create()
+        {
+            ViewData["TeamJobId"] = new SelectList(_context.TeamJobs, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Teams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ImagePath,Name,Surname,TeamJobId,Id,CreatedDate,UpdatedDate,DeletedDate")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeamJobId"] = new SelectList(_context.TeamJobs, "Id", "Id", team.TeamJobId);
+            return View(team);
+        }
+
+        // GET: Admin/Teams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            ViewData["TeamJobId"] = new SelectList(_context.TeamJobs, "Id", "Id", team.TeamJobId);
+            return View(team);
+        }
+
+        // POST: Admin/Teams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ImagePath,Name,Surname,TeamJobId,Id,CreatedDate,UpdatedDate,DeletedDate")] Team team)
+        {
+            if (id != team.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(team);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamExists(team.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeamJobId"] = new SelectList(_context.TeamJobs, "Id", "Id", team.TeamJobId);
+            return View(team);
+        }
+
+        // GET: Admin/Teams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.TeamJob)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // POST: Admin/Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamExists(int id)
+        {
+            return _context.Teams.Any(e => e.Id == id);
+        }
+    }
+}
